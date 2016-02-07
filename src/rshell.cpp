1cb461ccf36b0a47381de2220d796bbbf5513655
Jeffrey Hsieh <jhsie007@ucr.edu>
2016-02-06 21:56:03 -0800

    Created Classes

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -3,6 +3,22 @@
 
 using namespace std;
 
+//Base Class: Line
+class Line
+{
+	private:
+		vector<Line> data;
+	public:
+		void listener();
+		virtual void process();
+};
+
+//Inheritances: Solo, Duo, Connector
+class Solo: public Line
+{
+	public:
+		void process();
+};
 
 int main()
 {
